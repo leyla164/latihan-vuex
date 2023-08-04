17804ab3b6edacf45a47b02018ef5f9b5191964f
leyla164 <leylamaisarahdasni@gmail.com>
2023-08-04 15:04:12 +0700

    vuex

---
+++ src/assets/Kategori.js
@@ -0,0 +1,29 @@
+export const kategori = {
+    "kategori" : [
+        {
+            "id" : 1,
+            "nama" : "Aksesoris",
+            "img" : "aksesoris.jpg"
+        },
+        {
+            'id' : 2,
+            "nama" : "Elektronik",
+            "img" : "elektronik.jpg"
+        },
+        {
+            'id' : 3,
+            "nama" : "Pakaian Wanita",
+            "img" : "bajuwa.jpg"
+        },
+        {
+            'id' : 4,
+            "nama" : "Kosmetik",
+            "img" : "kosmetik.jpg"
+        },
+        {
+            'id' : 5,
+            "nama" : "Skincare",
+            "img" : "skincare.jpg"
+        },
+    ]
+}
---
+++ src/assets/Produk.js
@@ -0,0 +1,126 @@
+export const produk = {
+    "produk" : [
+        {
+            "id" : 1,
+            "nama" : "Topi",
+            "harga" : "25.000",
+            "id_kategori" : 1,
+            "img" : "topi.jpg",
+            "keterangan" : "Topi adalah suatu jenis penutup kepala. Penggunaan Topi dimaksudkan untuk beberapa alasan.Umumnya digunakan sebagai pelindung dari sinar matahari dan aksesoris pakaian.Kami menyediakan produk fashion wanita berupa aksesoris Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu "
+        },
+        {
+            'id' : 2,
+            "nama" : "Kalung",
+            "harga" : "30.000",
+            "id_kategori" : 1,
+            "img" : "kalung.jpg",
+            "keterangan" : "Kalung adalah benda atau barang berupa perhiasan yang dikenakan melingkari leher. Kami menyediakan produk fashion wanita berupa aksesoris Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 3,
+            "nama" : "TV",
+            "harga" : "3.000.000",
+            "id_kategori" : 2,
+            "img" : "tvss.jpg",
+            "keterangan" : "Televisi adalah alat yang mampu menerima data, berupa gambar dan suara secara bersamaan di waktu yang sama. Kami menyediakan produk elektronik seperti tv,hp,dan laptop. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+           
+        },
+        {
+            'id' : 4,
+            "nama" : "handphone",
+            "harga" : "3.000.000",
+            "id_kategori" : 2,
+            "img" : "dows.jpg",
+            "keterangan" : "handphone digunakan untuk komunikasi,memberi barang secara online,dan handphone suda digunakan dizaman sekarang. Kami menyediakan produk elektronik seperti tv,hp,dan laptop. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 5,
+            "nama" : "Laptop",
+            "harga" : "3.000.000",
+            "id_kategori" : 2,
+            "img" : "macbook.jpg",
+            "keterangan" : "Sebuah laptop adalah komputer pribadi yang dapat dipindahkan dan dibawa dengan mudah sehingga dapat digunakan di banyak tempat. Mayoritas laptop mempunyai fitur yang sama dengan komputer, seperti mampu menjalankan perangkat lunak dan mengelola berkas. Namun, laptop cenderung lebih mahal daripada komputer desktop. Kami menyediakan produk elektronik seperti tv,hp,dan laptop. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 6,
+            "nama" : "Anting",
+            "harga" : "5.000.000",
+            "id_kategori" : 1,
+            "img" : "flower.jpg",
+            "keterangan" : "Anting-anting adalah perhiasan telinga yang menggantung pada kuping telinga.Kami menyediakan produk fashion wanita berupa aksesoris Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 7,
+            "nama" : "Kemeja",
+            "harga" : "35.000",
+            "id_kategori" : 3,
+            "img" : "kemeja.jpg",
+            "keterangan" : " Kami menyediakan produk fashion wanita berupa aksesoris Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 8,
+            "nama" : "kaos",
+            "harga" : "25.000",
+            "id_kategori" : 3,
+            "img" : "korea.jpg",
+            "keterangan" : " Kami menyediakan produk fashion wanita berupa aksesoris Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 9,
+            "nama" : "Rok",
+            "harga" : "45.000",
+            "id_kategori" : 3,
+            "img" : "rok.jpg",
+            "keterangan" : " Kami menyediakan produk fashion wanita berupa aksesoris Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+
+        },
+        {
+            'id' : 10,
+            "nama" : "Bedak",
+            "harga" : "40.000",
+            "id_kategori" : 4,
+            "img" : "bedakk.jpg",
+            "keterangan" : " Kami menyediakan produk fashion wanita berupa aksesoris,kosmetik,dan skincare Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 11,
+            "nama" : "Maskara",
+            "harga" : "20.000",
+            "id_kategori" : 4,
+            "img" : "maskara.jpg",
+            "keterangan" : " Kami menyediakan produk fashion wanita berupa aksesoris,kosmetik,dan skincare Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 12,
+            "nama" : "Lipstick",
+            "harga" : "30.000",
+            "id_kategori" : 4,
+            "img" : "lipstik.jpg",
+            "keterangan" : " Kami menyediakan produk fashion wanita berupa aksesoris,kosmetik,dan skincare Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 13,
+            "nama" : "Moisturizer",
+            "harga" : "150.000",
+            "id_kategori" : 55,
+            "img" : "cerami.jpg",
+            "keterangan" : " Kami menyediakan produk fashion wanita berupa aksesoris,kosmetik,dan skincare Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 14,
+            "nama" : "Toner",
+            "harga" : "75.000",
+            "id_kategori" : 55,
+            "img" : "pyunkang.jpg",
+            "keterangan" : " Kami menyediakan produk fashion wanita berupa aksesoris,kosmetik,dan skincare Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+        {
+            'id' : 15,
+            "nama" : "Sunscreen",
+            "harga" : "25.000",
+            "id_kategori" : 55,
+            "img" : "wardah.jpg",
+            "keterangan" : " Kami menyediakan produk fashion wanita berupa aksesoris,kosmetik,dan skincare Semua produk didesain bergaya elegan sehingga nyaman digunakan Termukan outfit terbaikmu di sini!. Jam operasional Setiap hari : pukul 09.00 – 16.00 WIB Pengiriman : Senin – Sabtu"
+        },
+    ]
+}
